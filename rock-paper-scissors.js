a2b5f89eb63a3789977fd4de05ae6a2956ce238b
Mir Abrar Ahmed <abrarahmedshawn@gmail.com>
2022-02-05 02:44:14 +0600

    rock-paper-scissors game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -11,10 +11,11 @@ function roundPlay(playerSelection, computerSelection){
 	playerSelection = playerSelection.toLowerCase()	
 	winningMessage = 'You win! ' + playerSelection + ' beats ' + computerSelection
 	losingMessage = 'You lose! ' + computerSelection + ' beats ' + playerSelection
+	drawMessage = 'It is a draw'
 
 
 	if(playerSelection === computerSelection){
-		return 'It is a draw'
+		return drawMessage
 	} else if(playerSelection === 'rock'){
 		if(computerSelection === 'scissors'){
 			return winningMessage
@@ -39,9 +40,27 @@ function roundPlay(playerSelection, computerSelection){
 }
 
 function game(){
+	let playerWin = 0
+	let computerWin = 0
+	let drawCount = 0
 	for(i = 1; i <= 5; i++){
 		const playerSelection = prompt("Play your move:")
 		const computerSelection = computerPlay()
-		console.log(roundPlay(playerSelection, computerSelection))
+		round = roundPlay(playerSelection, computerSelection)
+		console.log(round)
+		if (round === winningMessage) {
+			playerWin += 1;
+		} else if(round === losingMessage) {
+			computerWin += 1;
+		} else {
+			drawCount += 1
+		}
+	}
+	if (playerWin > computerWin){
+		return "Player won!"
+	} else if(playerWin < computerWin){
+		return "Computer won!"
+	} else{
+		return "Nobody won"
 	}
 }
